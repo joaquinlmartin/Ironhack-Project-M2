7748bd037e48a670b47395bf5a64a7e6d3ff3df2
joaquinlmartin <joaquinlmartin@gmail.com>
2022-01-10 14:50:55 +0100

    seeds

--- seeds/cards.seed.js
+++ seeds/cards.seed.js
@@ -8,9 +8,9 @@ const cards = [
     { image: "https://img.pokemondb.net/artwork/large/pikachu.jpg", name: "Pikachu",  element: "electric", description: "When several of these POKKéMON gather, their electricity could build and cause lightning storms.", attack: 55, HP: 35, ability: "Static"},
     { image: "https://img.pokemondb.net/artwork/large/arbok.jpg", name: "Arbok",  element: "poison", description: "It is rumored that the ferocious warning markings on its belly differ from area to area.", attack: 95, HP: 60, ability: "Intimidate"},
     { image: "https://img.pokemondb.net/artwork/large/bulbasaur.jpg", name: "Bulbasaur",  element: "grass", description: "A strange seed was planted on its back at birth. The plant sprouts and grows with this POKKéMON.", attack: 49, HP: 45, ability: "Overgrow"},
-    { image: "https://img.pokemondb.net/artwork/large/squirtle.jpg", name: "Squirtle",  element: "water", description: "After birth, its back swells and hardens into a shell. Powerfully sprays foam from its mouth.", attack: 44, HP: 48, ability: "Torrent"},    
+    { image: "https://img.pokemondb.net/artwork/large/squirtle.jpg", name: "Squirtle",  element: "water", description: "After birth, its back swells and hardens into a shell. Powerfully sprays foam from its mouth.", attack: 44, HP: 48, ability: "Torrent"},
   ];
-
+  
   mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
