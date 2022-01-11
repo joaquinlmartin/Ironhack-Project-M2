a24f7f0374172838f6a22dff21d90dd2855a4150
joaquinlmartin <joaquinlmartin@gmail.com>
2022-01-11 20:17:47 +0100

    layout and script modified

--- middlewares/index.js
+++ middlewares/index.js
@@ -6,19 +6,3 @@ function isLoggedIn(req,res, next) {
 }
 
 module.exports = isLoggedIn;
-
-// const isLoggedIn = (req,res, next) => {
-//   if (!req.session.user) {
-//       return res.redirect('/login');
-//   }
-//   next();
-// };
-
-// const isLoggedOut = (req, res, next) => {
-//   if(req.session.user) {
-//       return res.redirect('/');
-//   }
-//   next();
-// };
-
-// module.exports = { isLoggedIn, isLoggedOut };
